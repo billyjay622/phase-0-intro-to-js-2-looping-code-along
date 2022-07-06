9635f8281afca7e9edcd6e43fde5e7d56eecf3e5
William Barr <williamjaybarr@gmail.com>
2022-07-06 16:56:51 -0400

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown () {
+    let i = 10 ;
+	while(i >= 0){
+		console.log(i)
+		i--;
+	}
+}	
+start(10)
+
+
+
+ 
+
